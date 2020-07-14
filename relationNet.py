d6c9b8627fe32e4bc159b2ee1c230705dbb1b606
SONG Sizhe <hereissausage@gmail.com>
2020-07-14 21:37:10 +0800

    Switched to Conv1d

--- relationNet.py
+++ relationNet.py
@@ -21,70 +21,30 @@ def weights_init(m):
         m.weight.data.normal_(0, 0.01)
         m.bias.data = torch.ones(m.bias.data.size())
 
-#TODO modify some constant
-
 # Relation Network Module
 class RelationNetwork(nn.Module):
-    def __init__(self,input_size,hidden_size):
+    def __init__(self,input_size, hidden_size):
         super(RelationNetwork, self).__init__()
         self.layer1 = nn.Sequential(
-                        nn.Conv2d(input_size*2,input_size,kernel_size=1,padding=0),
-                        nn.BatchNorm2d(input_size, momentum=1, affine=True),
+                        nn.Conv1d(input_size*2,input_size,kernel_size=1),
+                        nn.BatchNorm1d(input_size, momentum=1, affine=True),
                         nn.ReLU(),
-                        nn.MaxPool2d(2))
+                        nn.MaxPool1d(2))
         self.layer2 = nn.Sequential(
-                        nn.Conv2d(input_size,input_size,kernel_size=3,padding=0),
-                        nn.BatchNorm2d(input_size, momentum=1, affine=True),
+                        nn.Conv1d(input_size,input_size,kernel_size=3),
+                        nn.BatchNorm1d(input_size, momentum=1, affine=True),
                         nn.ReLU(),
-                        nn.MaxPool2d(2))
-        self.fc1 = nn.Linear(1024, 256)#input_size*3*3,hidden_size)
-        self.fc2 = nn.Linear(256, hidden_size)
-        self.fc3 = nn.Linear(hidden_size,1)
+                        nn.MaxPool1d(2))
+        self.fc1 = nn.Linear(75, hidden_size)
+        self.fc2 = nn.Linear(hidden_size,1)
 
-        # Initialize itself
-        self.apply(weights_init)
+        # # Initialize itself
+        # self.apply(weights_init)
 
-    def forward(self,x):
+    def forward(self,x):    
         out = self.layer1(x)
         out = self.layer2(out)
         out = out.view(out.size(0),-1)
         out = nn.functional.relu(self.fc1(out))
-        out = nn.functional.relu(self.fc2(out))
-        out = torch.sigmoid(self.fc3(out))
+        out = torch.sigmoid(self.fc2(out))
         return out
-
-class CNNEncoder(nn.Module):
-    """docstring for ClassName"""
-    def __init__(self):
-        super(CNNEncoder, self).__init__()
-        self.layer1 = nn.Sequential(
-                        nn.Conv2d(3,64,kernel_size=3,padding=0),
-                        nn.BatchNorm2d(64, momentum=1, affine=True),
-                        nn.ReLU(),
-                        nn.MaxPool2d(2))
-        self.layer2 = nn.Sequential(
-                        nn.Conv2d(64,64,kernel_size=3,padding=0),
-                        nn.BatchNorm2d(64, momentum=1, affine=True),
-                        nn.ReLU(),
-                        nn.MaxPool2d(2))
-        self.layer3 = nn.Sequential(
-                        nn.Conv2d(64,64,kernel_size=3,padding=1),
-                        nn.BatchNorm2d(64, momentum=1, affine=True),
-                        nn.ReLU())
-        self.layer4 = nn.Sequential(
-                        nn.Conv2d(64,64,kernel_size=3,padding=1),
-                        nn.BatchNorm2d(64, momentum=1, affine=True),
-                        nn.ReLU())
-
-        # Initialize itself
-        self.apply(weights_init)
-
-    def forward(self,x):
-        #print(x.size()) # size = [10, 3, 256, 256]
-        out = self.layer1(x)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        #out = out.view(out.size(0),-1)
-        #print(out.size())
-        return out # size = [10, 64, 62, 62]
